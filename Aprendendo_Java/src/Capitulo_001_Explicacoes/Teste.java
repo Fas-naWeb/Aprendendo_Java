1433ab613808478942bb31f9ce5a615e1ed88816
faswe <faswe@192.168.0.4>
2021-02-07 00:03:27 -0300

    Aprendendo a utilizar o printf.

---
+++ Aprendendo_Java/src/Capitulo_001_Explicacoes/Teste.java
@@ -0,0 +1,21 @@
+package Capitulo_001_Explicacoes;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		//Imprimindo uma linha de texto com multiplas instruções.
+		System.out.print("\"Meu primeiro");
+		System.out.println(" programa java!!!\"");
+		
+		//Dividindo meu nome com caractere de escape \n
+		System.out.print("Fabiano\nAlves\nSantos\"\n\n");
+		
+		//---------------------------------------------------------------
+		
+		//Imprimindo texto com printf
+		System.out.printf("%s%s%s","Estou ","Aprendendo ","Java!!!");
+		
+		
+	}
+
+}
