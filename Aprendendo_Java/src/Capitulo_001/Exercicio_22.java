be984a3f4b449bd7c7a8d70695190ded2b5b7ed2
faswe <faswe@DESKTOP-IG35LSE>
2021-02-07 15:28:22 -0300

    Exercício de revisão 2.2

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_22.java
@@ -0,0 +1,28 @@
+package Capitulo_001;
+
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 07/02/2021 
+*/
+
+public class Exercicio_22 {
+/*
+	Determine se cada uma das seguintes afirmações é verdadeira ou falsa. Se falsa, explique por quê.
+	
+	a) Os comentários fazem com que o computador imprima o texto depois das // na tela quando o programa executa.
+	    R: Falsa tudo que vem depois do // é comentários de uma linha, não é impresso nada na tela.
+	    
+	b) Todas as variáveis devem ser atribuídas a um tipo quando são declaradas.
+		R: Verdadeira
+	
+	c) O Java considera que as variáveis number e NuMbEr são idênticas.
+		R:Falso, java faz distinção entre maiúsculas e minusculas.
+	
+	d) O operador de resto (%) pode ser utilizado apenas com operandos inteiros.
+		R:Falso, o operador resto (%) pode ser usado com tipos ponto flutuante também.
+		
+	e) Os operadores aritméticos *, /, %, + e - têm, todos, o mesmo nível de precedência.
+		R:Falso operadores como *, /, % são executados primeiro do que os operadores de + e -
+*/
+}
