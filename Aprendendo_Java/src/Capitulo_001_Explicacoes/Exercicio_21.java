621bb7e8a7f2d0702484beaa0f1bb69bbfb82751
faswe <faswe@DESKTOP-IG35LSE>
2021-02-07 15:00:03 -0300

    Exercicio de revisão 2.1

---
+++ Aprendendo_Java/src/Capitulo_001_Explicacoes/Exercicio_21.java
@@ -0,0 +1,30 @@
+package Capitulo_001_Explicacoes;
+
+/*
+ * Autor:Fabiano Alves Santos
+ * Email:fasnaweb2020@gmail.com
+ * Data: 07/02/2020 
+ */
+
+public class Exercicio_21 {
+/*
+	Preencha as lacunas em cada uma das seguintes afirmações:
+		a) Um(a) ________ começa o corpo de cada método e um(a) ________ termina o corpo de cada método.
+		b) Você pode usar a declaração ________ para tomar decisões.
+		c) ________ começa em um comentário de fim de linha.
+		d) ________, ________ e ________ são chamados espaço em branco.
+		e) ________ são reservadas para uso pelo Java.
+		f) Aplicativos Java iniciam a execução no método ________.
+		g) Os métodos ________, ________ e ________ exibem informações em uma janela de comando.
+*/
+/*
+	    a)Chave para esquerda ( { ),Chave para direita ( } )
+	    b)Declaração if
+	    c)// é utilizada para comentario de fim de linha
+	    d)\n,\t e espaço em branco
+	    e)Palavra chave
+	    f)Main ou public static void Main(String[] args){ }
+	    g)print, println e printf.
+ */
+	
+}
