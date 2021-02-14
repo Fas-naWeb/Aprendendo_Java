f06c941c50ab8eb785ba58dadcb3152b0da87611
fabiano <fabiano@fabiano-IS-1422>
2021-02-14 00:19:35 -0300

    Exercicio de revisão 2.6

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_26.java
@@ -0,0 +1,31 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 13/02/2021 
+*/
+import java.util.Scanner;
+
+public class Exercicio_26 {
+
+	public static void main(String[] args) {
+		 //Declare que um programa calculará o produto de três inteiros.
+	    Scanner entradas = new Scanner(System.in);
+	     int x,y,z, result;
+	     
+	     System.out.println("Entre com o primeiro valor inteiro:");
+	     x = entradas.nextInt();
+	     
+	     System.out.println("Entre com o segundo valor inteiro:");
+	     y = entradas.nextInt();
+	     
+	     System.out.println("Entre com o terceiro valor inteiro:");
+	     z = entradas.nextInt();
+	     
+	     result = x * y * z;
+	     
+	     System.out.printf("Produto é: %d",result);
+
+	}
+
+}
