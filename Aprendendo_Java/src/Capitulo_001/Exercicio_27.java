795c178617fe1b54f00aea2032e4e1830dfe08d1
fabiano <fabiano@fabiano-IS-1422>
2021-02-14 00:31:41 -0300

    Exercício de revisão 2.7

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_27.java
@@ -0,0 +1,27 @@
+package Capitulo_001;
+
+public class Exercicio_27 {
+/*
+	Preencha as lacunas em cada uma das seguintes afirmações:
+		a) ________ são utilizados para documentar um programa e aprimorar sua legibilidade.
+		R:Comentário de documentação /**
+		                               * .....
+		                               */
+/*		b) Uma decisão pode ser tomada em um programa Java com um(a) ________ .
+        R: Estrutura seleção if(){}
+        
+		c) Os cálculos normalmente são realizados pelas instruções ________ .
+		R:Ijnstruções de soma(+),subtração(-),divisão(/),multiplicação(*),resto(%).
+		
+		d) Os operadores aritméticos com a mesma precedência da multiplicação são ________ e ________ .
+		R:/ e %.
+		
+		e) Quando parênteses em uma expressão aritmética estão aninhados, o conjunto de parênteses ________ é avaliado primeiro.
+		R:Mais interno é avaliado primeiro.
+		
+		f) Uma posição na memória do computador que pode conter valores diferentes várias vezes ao longo da execução de um program
+        R: Valor destrutivo.
+*/
+	
+
+}
