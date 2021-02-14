18fe4ff1bad3e252d3983c0bc0074ab96c825ce4
fabiano <fabiano@fabiano-IS-1422>
2021-02-14 00:48:36 -0300

    Exercício de revisão 2.8

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_28.java
@@ -0,0 +1,19 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 13/02/2021 
+*/
+public class Exercicio_28 {
+
+	public static void main(String[] args) {
+/*
+    Escreva instruções Java que realizem cada uma das seguintes tarefas:
+	a) Exibir a mensagem “Enter an integer: “ , deixando o cursor na mesma linha.
+	b) Atribuir o produto de variáveis b e c para a variável a .
+	c) Utilizar um comentário para afirmar que um programa executa um cálculo de exemplo de folha de pagamento.
+*/
+
+	}
+
+}
