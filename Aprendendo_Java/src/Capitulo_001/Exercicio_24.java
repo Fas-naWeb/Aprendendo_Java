ddf59c3f9eff3c29dcc5ac890d3e084beaa427be
fabiano <fabiano@fabiano-IS-1422>
2021-02-13 23:50:11 -0300

    Exerçicio de revisão 2.4

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_24.java
@@ -0,0 +1,22 @@
+package Capitulo_001;
+
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 13/02/2021 
+*/
+
+
+public class Exercicio_24 {
+/*
+	Identifique e corrija os erros em cada uma das seguintes instruções:
+	a) if (c < 7);		
+	System.out.println(“c is less than 7”);
+	R:Ponto e virgula indica final de instrução,não deveria esta depois dos parenteses da intrução if.
+	
+	b) if (c => 7)
+	System.out.println(“c is equal to or greater than 7”);
+	R:O operador relacional => esta errado,o certo seria >= .
+ */
+
+}
