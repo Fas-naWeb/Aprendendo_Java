7333e38daaa492d90a195e23ffae81339413935c
fabiano <fabiano@fabiano-IS-1422>
2021-02-14 00:07:46 -0300

    Exercício de revisão 2.5

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_25.java
@@ -0,0 +1,43 @@
+package Capitulo_001;
+import java.util.Scanner;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 13/02/2021 
+*/
+public class Exercicio_25 {
+/*
+	Escreva declarações, instruções ou comentários que realizem cada uma das tarefas a seguir:
+		a) Declare que um programa calculará o produto de três inteiros.
+		b) Crie um Scanner chamado input que leia valores a partir da entrada padrão.
+		c) Declare as variáveis x , y , z e result como tipo int .
+		d) Solicite que o usuário insira o primeiro inteiro.
+		e) Leia o primeiro inteiro digitado pelo usuário e armazene-o na variável x .
+		f) Solicite que o usuário insira o segundo inteiro.
+		g) Leia o segundo inteiro digitado pelo usuário e armazene-o na variável y .
+		h) Solicite que o usuário insira o terceiro inteiro.
+		i) Leia o terceiro inteiro digitado pelo usuário e armazene-o na variável z .
+		j) Compute o produto dos três inteiros contidos nas variáveis x , y e z e atribua o resultado à variável result 
+		k) Use System.out.printf para exibir a mensagem “Product is ” seguida pelo valor da variável result.
+*/
+	public static void main(String[] args) {
+		//Declare que um programa calculará o produto de três inteiros.
+	     Scanner entradas = new Scanner(System.in);
+	     int x,y,z, result;
+	     
+	     System.out.println("Entre com o primeiro valor inteiro:");
+	     x = entradas.nextInt();
+	     
+	     System.out.println("Entre com o segundo valor inteiro:");
+	     y = entradas.nextInt();
+	     
+	     System.out.println("Entre com o terceiro valor inteiro:");
+	     z = entradas.nextInt();
+	     
+	     result = x * y * z;
+	     
+	     System.out.printf("Produto é: %d",result);
+	  
+	}
+
+}
