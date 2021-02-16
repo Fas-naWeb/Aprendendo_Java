5818bd0a20cbdd2393beb00ce667771939754b66
fabiano <fabiano@fabiano-IS-1422>
2021-02-16 01:10:31 -0300

    Exercício de revisão 2.9

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_29.java
@@ -0,0 +1,23 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 13/02/2021 
+*/
+public class Exercicio_29 {
+/*
+	Determine se cada uma das seguintes afirmações é verdadeira ou falsa. Se falsa, explique por quê.
+	a) Operadores Java são avaliados da esquerda para a direita.
+	   R:Verdadeiro
+	   
+	b) Os seguintes nomes são todos de variável válidos: _under_bar_ , m928134 , t5 , j7 , her_sales$ , his_$account_total , a , b$ ,
+	   c , z e z2 .
+	   R:Verdadeiro
+	   
+	c) Uma expressão aritmética Java válida sem parênteses é avaliada da esquerda para a direita.
+	   R:Verdadeira
+	   
+	d) Os seguintes nomes são todos de variável inválidos: 3g , 87 , 67h2 , h22 e 2h .
+	   R:Falso,nomes de variáveis não pode iniciar com números.
+*/
+}
