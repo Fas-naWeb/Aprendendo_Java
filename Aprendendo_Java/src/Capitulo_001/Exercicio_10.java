1fd9059b3f461640db3223070bdd9a5549b04a9c
fabiano <fabiano@fabiano-IS-1422>
2021-02-16 01:30:17 -0300

    Exercício de revisão 2.10

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_10.java
@@ -0,0 +1,23 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 13/02/2021 
+*/
+public class Exercicio_10 {
+/*
+	Supondo que x = 2 e y = 3 , o que cada uma das instruções a seguir exibe?
+			a) System.out.printf(“x = %d%n”, x);
+			   R:X = 2
+			   
+			b) System.out.printf(“Value of %d + %d is %d%n”, x, x, (x + x));
+			   R:Value of 2 + 2 is 4
+			   
+			c) System.out.printf(“x =”);
+			   R:X =
+			   
+			d) System.out.printf(“%d = %d%n”, (x + y), (y + x));
+			   R:5 = 5
+*/
+	
+}
