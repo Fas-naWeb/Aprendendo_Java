b3605262cbbb9600287574b198276e9fb1c11fb4
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 15:21:20 -0300

    Exercicio de revisão 19

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_019.java
@@ -0,0 +1,17 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 20/02/2021 
+*/
+public class Exercicio_019 {
+/*
+	O que o seguinte código imprime?
+	System.out.printf( "*%n**%n***%n****%n*****%n" );
+*/
+	//R: Imprime um Triangulo retangulo
+	
+	public static void main(String[] args) {
+		System.out.printf( "*%n**%n***%n****%n*****%n");
+	}
+}
