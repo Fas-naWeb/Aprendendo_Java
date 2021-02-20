e6c9ecdfc694ccc4364b11dbe1983713ea829792
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 15:35:19 -0300

    Exercicio de revisão 22

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_022.java
@@ -0,0 +1,26 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 20/02/2021 
+*/
+public class Exercicio_022 {
+/*
+	O que o seguinte código imprime?
+			System.out.print( "*" );
+			System.out.println( "***" );Capítulo 2
+			System.out.println( "*****" );
+			System.out.print( "****" );
+			System.out.println( "**" );
+			R: Três linhas de *
+*/
+	public static void main(String[] args) {
+		System.out.print( "*" );
+		System.out.println( "***" );
+		System.out.println( "*****" );
+		System.out.print( "****" );
+		System.out.println( "**" );
+
+	}
+
+}
