d8265fc3f59251498034b299439af59985f60e8b
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 13:48:12 -0300

    Exercício de revisão 2.17

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_217.java
@@ -0,0 +1,14 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 19/02/2021 
+*/
+public class Exercicio_217 {
+/*
+	(Aritmética, menor e maior) Escreva um aplicativo que insira três inteiros digitados pelo usuário e exiba a soma, média, produto e os
+	números menores e maiores. Utilize as técnicas mostradas na Figura 2.15. [Observação: o cálculo da média neste exercício deve resultar
+	em uma representação de inteiro. Assim, se a soma dos valores for 7, a média deverá ser 2, não 2,3333...]
+*/
+}
+ 
