212a4b893be424f6bb0ea46f8ddecfa6ee62d1ca
fabiano <fabiano@fabiano-IS-1422>
2021-02-19 23:33:51 -0300

    Exercício de revisão 2.14

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_214.java
@@ -0,0 +1,22 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 19/02/2021 
+*/
+public class Exercicio_214 {
+/*
+	Escreva um aplicativo que exiba os números 1 a 4 na mesma linha, com cada par de adjacentes separados por um espaço. Use as seguintes
+	técnicas:
+	a) Uma instrução System.out.println .
+	b) Quatro instruções System.out.print .
+	c) Uma instrução System.out.printf .
+*/
+	public static void main(String[] args) {
+		System.out.print("1 - ");
+		System.out.printf("%d - %d -", 2,3);
+		System.out.println(" 4");
+	}
+	
+	
+}
