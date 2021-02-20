c06e5870be97bf01f5632556a7cb63910f29681d
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 14:07:39 -0300

    Exercicio de revisão 6

--- Aprendendo_Java/src/Capitulo_001/Exercicio_6.java
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_6.java
@@ -6,7 +6,7 @@ package Capitulo_001;
 */
 import java.util.Scanner;
 
-public class Exercicio_26 {
+public class Exercicio_6 {
 
 	public static void main(String[] args) {
 		 //Declare que um programa calculará o produto de três inteiros.
