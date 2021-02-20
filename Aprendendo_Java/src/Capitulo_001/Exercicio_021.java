d86658366f8bc38fbdd9ed14cb6633d8a5f047a5
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 15:30:09 -0300

    Exercicio de revisão 21

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_021.java
@@ -0,0 +1,23 @@
+package Capitulo_001;
+
+public class Exercicio_021 {
+/*
+	O que o seguinte código imprime?
+			System.out.print( "*" );
+			System.out.print( "***" );
+			System.out.print( "*****" );
+			System.out.print( "****" );
+			System.out.println( "**" );
+			R:Uma linha de *
+*/
+	public static void main(String[] args) {
+		
+		System.out.print( "*" );
+		System.out.print( "***" );
+		System.out.print( "*****" );
+		System.out.print( "****" );
+		System.out.println( "**" );
+
+	}
+
+}
