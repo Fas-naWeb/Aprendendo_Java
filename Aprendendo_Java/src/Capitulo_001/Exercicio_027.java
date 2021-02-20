c836d91b61f616c22802e9a823157e6fb19b5440
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 20:51:49 -0300

    Exercicio de revizão 27

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_027.java
@@ -0,0 +1,19 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 20/02/2021 
+*/
+public class Exercicio_027 {
+/*
+	(Padrão de tabuleiro de damas de asteriscos) Escreva um aplicativo que
+	 exiba um padrão de tabuleiro de damas, como mostrado a seguir:
+*/
+	public static void main(String[] args) {
+	  for(int i = 0;i < 4;i++) {
+	   System.out.printf("%s %s %s %s %s %s %s %s\n",'*','*','*','*','*','*','*','*');
+	   System.out.printf(" %s %s %s %s %s %s %s %s\n",'*','*','*','*','*','*','*','*');
+	  }
+	}
+
+}
