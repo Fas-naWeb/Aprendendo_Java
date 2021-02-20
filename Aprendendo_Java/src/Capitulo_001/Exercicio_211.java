3f9a7d95a17cbee428f7259f7bcde8cf33eb8c0f
fabiano <fabiano@fabiano-IS-1422>
2021-02-19 22:47:38 -0300

    Exercício de revisão 2.11

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_211.java
@@ -0,0 +1,13 @@
+package Capitulo_001;
+
+public class Exercicio_211 {
+/*
+	Quais instruções Java a seguir contêm variáveis cujos valores são modificados?
+			a) p = i + j + k + 7;
+			b) System.out.println(“variables whose values are modified”);
+			c) System.out.println(“a = 5”);
+			d) value = input.nextInt();
+			
+			R:Somente a letra (a) e a letra (d)
+*/
+}
