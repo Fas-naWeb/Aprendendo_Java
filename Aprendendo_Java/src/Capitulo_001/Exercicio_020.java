187c81ecb8f031d285f761ae1fe75ec80c91d86f
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 15:25:46 -0300

    Exercicio de revisão 20

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_020.java
@@ -0,0 +1,22 @@
+package Capitulo_001;
+
+public class Exercicio_020 {
+/*
+	O que o seguinte código imprime?
+			System.out.println( "*" );
+			System.out.println( "***" );
+			System.out.println( "*****" );
+			System.out.println( "****" );
+			System.out.println( "**" );
+			R:Uma nave kkkk...
+*/
+	public static void main(String[] args) {
+		
+				System.out.println( "*" );
+				System.out.println( "***" );
+				System.out.println( "*****" );
+				System.out.println( "***" );
+				System.out.println( "*" );
+	}
+
+}
