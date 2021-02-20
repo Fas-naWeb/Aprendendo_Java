e7f95de32a5e9e08acba183e1067329089c51f58
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 00:17:07 -0300

    Exercício de revisão 2.16

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_215.java
@@ -0,0 +1,30 @@
+package Capitulo_001;
+import java.util.Scanner;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 19/02/2021 
+*/
+public class Exercicio_215 {
+/*
+	(Aritmética) Escreva um aplicativo que solicite ao usuário inserir dois inteiros, obtenha dele esses números e imprima sua soma, produto,
+	diferença e quociente (divisão). Utilize as técnicas mostradas na Figura 2.7.
+*/
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+        int valor = 0;
+        
+		System.out.print("Digite um valor inteiro:");
+		valor = entrada.nextInt();
+		
+		System.out.printf("%d + %d = %d\n",valor,valor,(valor + valor));
+		System.out.printf("%d - %d = %d\n",valor,valor,(valor - valor));
+		System.out.printf("%d * %d = %d\n" ,valor,valor,(valor * valor));
+		System.out.printf("%d / %d = %d\n",valor,valor,(valor / valor));
+		System.out.printf("Resto = %d",(valor % valor));
+		
+		
+	}
+}
---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_216.java
@@ -0,0 +1,29 @@
+package Capitulo_001;
+import java.util.Scanner;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 19/02/2021 
+*/
+public class Exercicio_216 {
+/*
+   (Comparando inteiros) Escreva um aplicativo que solicite ao usuário inserir dois inteiros, obtenha dele esses números e exiba o número
+	maior seguido pelas palavras “is larger" . Se os números forem iguais, imprima a mensagem “These numbers are equal" . Utilize
+	as técnicas mostradas na Figura 2.15.
+*/
+	public static void main(String[] args) {
+		Scanner entradas = new Scanner(System.in);
+		
+		System.out.println("Entre com o primeiro valor:");
+		int valor1 = entradas.nextInt();
+		
+		System.out.println("Entre com o segundo valor:");
+		int valor2 = entradas.nextInt();
+		
+		if(valor1 > valor2) {
+			System.out.printf("Primeiro valor é maior do que o segundo,%d é > %d",valor1,valor2);
+		}else if(valor1 == valor2) {
+			System.out.printf("Os valores são iguais,%d é = %d",valor1,valor2);
+		}
+	}
+}
