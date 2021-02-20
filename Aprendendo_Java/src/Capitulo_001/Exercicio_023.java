15262bcf6a2bc463d2f14e8a23f8f3e6a9e87ebe
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 15:38:42 -0300

    Exercicio de revisão 23

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_023.java
@@ -0,0 +1,18 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 20/02/2021 
+*/
+public class Exercicio_023 {
+/*
+	O que o seguinte código imprime?
+			System.out.printf( "%s%n%s%n%s%n", "*", "***", "*****" );
+			R:Triangulo.
+*/
+	public static void main(String[] args) {
+	
+		System.out.printf( "%s%n%s%n%s%n", "*", "***", "*****" );
+	}
+
+}
