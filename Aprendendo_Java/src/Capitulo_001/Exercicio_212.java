e03a070e9f9dd11ea74d7aff0c010d22fa2f59f1
fabiano <fabiano@fabiano-IS-1422>
2021-02-19 22:55:56 -0300

    Exercício de revisão 2.12

---
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_212.java
@@ -0,0 +1,19 @@
+package Capitulo_001;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 07/02/2021 
+*/
+public class Exercicio_212 {
+/*
+ 	Dado que y = ax³+ 7, quais das seguintes alternativas são instruções Java corretas para essa equação?
+ 
+			a) y = a * x * x * x + 7;
+			b) y = a * x * x * (x + 7);
+			c) y = (a * x) * x * (x + 7);
+			d) y = (a * x) * x * x + 7;
+			e) y = a * (x * x * x) + 7;
+			f) y = a * x * (x * x + 7);
+			R:Letra (e).
+*/
+}
