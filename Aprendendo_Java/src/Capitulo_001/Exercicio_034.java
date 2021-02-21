e4e049637dfadb1d6c5b2184a7b80dd96cbce80c
fabiano <fabiano@fabiano-IS-1422>
2021-02-21 16:05:10 -0300

    Exercicio de revisão 34 -> Calculadora de crescimento demografio mundial

--- Aprendendo_Java/src/Capitulo_001/Exercicio_034.java
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_034.java
@@ -8,7 +8,7 @@ public class Exercicio_034 {
 /*
 	(Calculadora de crescimento demográfico mundial) Utilize a internet para descobrir a população mundial atual e a taxa de cresci-
 	mento demográfico mundial anual. Escreva um aplicativo que introduza esses valores e, então, que exiba a população mundial estimada
-	depois de um, dois, três, quatro e cinco anos.
+	depois de um, dois, três, quatro e cinco anos
 */
 	public static void main(String[] args) {
 		
