83c4276777ebeecbd2019f32edca2a320a7a768b
fabiano <fabiano@fabiano-IS-1422>
2021-02-20 21:29:34 -0300

    Exercicio de revisão 28

--- Aprendendo_Java/src/Capitulo_001/Exercicio_028.java
+++ Aprendendo_Java/src/Capitulo_001/Exercicio_028.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 	Email:fasnaweb2020@gmail.com
 	Data: 13/02/2021 
 */
-public class Exercicio_28 {
+public class Exercicio_028 {
 
 	public static void main(String[] args) {
 /*
