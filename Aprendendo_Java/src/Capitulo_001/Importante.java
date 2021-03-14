8bd810494b1eb6bd8c9a3aababa57f3db62cd7c1
fabiano <fabiano@fabiano-IS-1422>
2021-03-14 18:04:03 -0300

    Tpicos importantes

---
+++ Aprendendo_Java/src/Capitulo_001/Importante.java
@@ -0,0 +1,19 @@
+package Capitulo_001;
+
+public class Importante {
+/*
+ 1)método static é especial, porque você pode chamá-lo sem antes criar um objeto
+   da classe na qual esse método é declarado.
+   
+ 2)Classes consideradas no mesmo pacote são conhecidas como no pacote padrão. Classes no mesmo pacote são
+   importadas implicitamente para os arquivos de código-fonte de outras classes nesse pacote. Assim, uma declaração
+   import não é necessária quando uma classe adota outra no mesmo pacote
+   
+   Duas formas de importar classes no java
+   
+   import java.util.Scanner 
+   ou no corpo do programa
+   java.util.Scanner input = new java.util.Scanner(System.in);
+*/
+	
+}
