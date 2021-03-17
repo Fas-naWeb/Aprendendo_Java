76bc5f64efdc9630473d128949ac6fdca0194cbb
oem <fasbaweb2020@gmail.com>
2021-03-17 00:00:41 -0300

    Aprendendo Java pela Digital Innovation One

--- Aprendendo_Java/src/DigitalInnovationOne/Treinamento.java
+++ Aprendendo_Java/src/DigitalInnovationOne/Treinamento.java
@@ -1,7 +1,78 @@
 package DigitalInnovationOne;
-
+/*
+    Autor:Fabiano Alves Santos
+    Email:fasnaweb2020@gmail.com
+    Data:16/03/2021s
+*/
 public class Treinamento {
-    public static void main(String[] args){
-        System.out.println("Hellho Word!!");
-    }
+   /*
+     PREPARANDO O AMBIENTE PARA DESENVOLVER COM JAVA
+      1) Instalar a versão mais recente do java
+      2) sudo apt update -> fas uma atualização do sistema operacional
+      3) java --version -> verifica a versão instalada do java
+      4) Instalar o Gradle Build tool - mais recente
+      5) Instalar o Maven - Apache mais recente
+      6) Intellij Ide de desenvolvimento
+
+      INICIAR UM PROJETO USANDO SPRING BOOT
+      Entrar no site https://start.spring.io/
+
+      O QUE É JAVA ?
+      Java é uma linguagem de programação e plataforma computacional lançada em 1995 pela Sun Microsystems,por um time
+      comandado por James Gosling.Anos depois foi adquirida pela oracle.
+
+      Diferentes de outras linguagens de programação,que são compiladas para código nativo ,o java é compilado para um
+      bytecode que é interpretado por uma máquina virtual.
+
+      O QUE É UM COMPILADOR ?
+      Um compilador é um programa que,a partir de um código fonte,cfria um programa semanticamente equivalente,porém es-
+      crito em outra linguagem,código objeto.Um compilador traduz  um programa de uma linguagem textual para uma lingua-
+      gem de máquina,especifica para um processador e sistema operacional.
+
+      O nome compilador é usado principalmente para os programas que traduzem o código fonte de uma linguagem de progra-
+      mação de alto nível para uma linguagem de baixo nível exemplo,assembly ou linguagem de máquina
+
+      O QUE É BYTECODE ?
+      É o código originado da compilação de programas java.
+      O bytecode é o programa interpretado e executado pela maquina virtual java JVM.
+
+      O QUE É UMA VM ?
+      Uma máquina virtual é um software que simula uma máquina física e consegue executar vários programas ,gerenciar
+      processos, e memória e arquivos .Tudo  isso faz parte de uma plataforma com memória ,processador e outros  recur-
+      sos totalmente virtuais sem dependencia do hardware.
+
+      O QUE É UMA MVJ ?
+      A JVM é uma máquina virtual que executa programas java,executando os bytcodes em linguagem  de máquina para cada
+      sistema operacional
+
+      em linguagens compiladas diretamente para um sistema especifico ,esse programa não ira executar em outro S.O
+      havenmdo a necessidade de compilar uma versão do software para cada S.O
+
+      Com o java,compilamos para a jvm, o bytecode será executado pela máquina virtual,e não diretamente pelo S.O assim
+      o software escrito em java possui portabilidade para qualquer sistema operacianal ,porem cada jvm deve ser constru-
+      ida para umS.O especifico.
+
+      O QUE È A JRE ?
+      Ambiente de execução java,é composto pela jvm,bibliotecas e APIS da linguagem java e outros componentes para suporte
+      da plataforma java.
+      Ele representa a parte responsavel pela execução do software  java.
+
+      O QUE È A JDK ?
+      Kit de desenvolvimento java,é um conjunto de utilitários que permitem  criar  softwares para a plataforma java
+      É composto pelo compilador  java ,bibliotecas   da linguagem ,ferramentas e jre.
+
+      O QUE È A JSE ?
+      É a distribuição minima da plataforma de desenvolvimento de aplicações java.
+
+      OpemJdk é a implementação de referencia opensourse da plataforma java ,javaSe que é mantida pela oracle·
+
+      O QUE È JAVA EE ?
+      É uma extenção do java Se que possui suporte a desenvolvimento de sistemas corporativos.
+      Além do minimo  da plataforma,o java EE possui diversas especificações  de partes da infra estrutura de aplicações
+      como acesso a banco de dados,mensageris,serviços web,parser de arquivos e outras.
+
+      Servidores de aplicações java EE,sabem seguir essas expecificações e implementar os recursos para os usuários.
+
+
+   */
 }
