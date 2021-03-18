0adba5f5f1a08c4efae5cbea361f79ecc44a6629
oem <fasbaweb2020@gmail.com>
2021-03-18 01:00:15 -0300

    Treinamento Digital Innovation One

---
+++ Aprendendo_Java/src/DigitalInnovationOne/TiposDeDadosJava.java
@@ -0,0 +1,86 @@
+package DigitalInnovationOne;
+
+import java.sql.Wrapper;
+
+/*
+    Autor:Fabiano Alves Santos
+    Email:fasnaweb2020@gmail.com
+    Data:17/03/2021s
+*/
+public class TiposDeDadosJava {
+
+        //INTEIROS
+
+        //byte b = null; Nunca pode ser nulo
+
+        byte b;    //8 bits
+        byte b1 = 127;
+        byte b2 = -128;
+
+        char c;    //16 bits
+        char c1 = 'A';
+        char c2 = 15;
+
+        short s;   //16 bits
+        short s1 = 32767;
+        short s2 = -3278;
+
+        int i;     //32 bits
+        int i1 = 2147483647;
+        int i2 = -2147483648;
+
+        long l;     //64 bits
+        long l1 = 9223372036854775807L;
+        long l2 = -9223372036845775808L;
+
+        //FLUTUANTES
+
+        float f = 65f;   //32 bits
+        float f1 = 65.0f;
+        float f2 = -0.5f;
+
+        double d; //64 bits
+        double d1 = 1024.99;
+        double d2 = 10.2156;
+
+        // WRAPPER
+        // São objetos que representam os primitivos
+        // Auto boxing e um boxing
+
+        Byte B;    //8 bits
+        Byte B1 = 127;
+        Byte B2 = -128;
+
+        Character C;    //16 bits
+        Character C1 = 'A';
+        Character C2 = 15;
+
+
+        Short S;   //16 bits
+        Short S1 = 32767;
+        Short S2 = -3278;
+
+        Int I;     //32 bits
+        Int I1 = 2147483647;
+        Int I2 = -2147483648;
+
+        Long L;     //64 bits
+        Long L1 = 9223372036854775807L;
+        Long L2 = -9223372036845775808L;
+
+        //FLUTUANTES
+
+        Float F = 65f;   //32 bits
+        Float F1 = 65.0f;
+        Float F2 = -0.5f;
+
+        Double D; //64 bits
+        Double D1 = 1024.99;
+        Double D2 = 10.2156;
+
+        Boolean O;
+        Boolean O1 = true;
+        Boolean O2 = false;
+    }
+
+
