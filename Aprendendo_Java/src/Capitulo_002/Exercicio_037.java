363c4eb6746d12b35f66b1d2dfcb91b5c552de54
oem <fasbaweb2020@gmail.com>
2021-03-30 12:28:06 -0300

    Exercicio de revisão 037

---
+++ Aprendendo_Java/src/Capitulo_002/Exercicio_037.java
@@ -0,0 +1,14 @@
+package Capitulo_002;
+/*
+   Autor:Fabiano Alves Santos
+   Email:fasnaweb2020@gmail.com
+   Data: 30/03/2021
+*/
+public class Exercicio_037 {
+/*
+ 	Explique o propósito de um parâmetro de método. Qual a diferença entre um parâmetro e um argumento?
+ 	R:Um parâmetro representa informações adicionais que um método requer para realizar sua tarefa. Cada parâmetro requerido por um
+      método é especificado na declaração do método. Um argumento é o valor real de um parâmetro de método. Quando um método é cha-
+      mado, os valores de argumento são passados para os parâmetros correspondentes desse método para que ele possa realizar sua tarefa.
+ */
+}
