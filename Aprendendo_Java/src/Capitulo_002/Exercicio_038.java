2a62c6dc442f8e53dd068ddb3dd21236e3a79cf6
oem <fasbaweb2020@gmail.com>
2021-03-30 13:11:05 -0300

    Exercicio de revisão 038

---
+++ Aprendendo_Java/src/Capitulo_002/Exercicio_038.java
@@ -0,0 +1,12 @@
+package Capitulo_002;
+/*
+   Autor:Fabiano Alves Santos
+   Email:fasnaweb2020@gmail.com
+   Data: 30/03/2021
+*/
+public class Exercicio_038 {
+  //(Palavra-chave new ) Qual é o objetivo da palavra-chave new ? Explique o que acontece quando você a utiliza.
+    /*
+      A palavra chave new tem por objetivo instanciar um objeto (cria um objeto).
+    */
+}
