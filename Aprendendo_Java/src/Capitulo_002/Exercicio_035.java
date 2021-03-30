ab365985b836d224f6e0531100f15637c11c246f
oem <fasbaweb2020@gmail.com>
2021-03-30 12:23:41 -0300

    Exercicio de revisão 035

---
+++ Aprendendo_Java/src/Capitulo_002/Exercicio_035.java
@@ -0,0 +1,44 @@
+package Capitulo_002;
+/*
+   Autor:Fabiano Alves Santos
+   Email:fasnaweb2020@gmail.com
+   Data: 30/03/2021
+*/
+public class Exercicio_035 {
+/*
+        Determine se cada uma das seguintes sentenças é verdadeira ou falsa. Se falsa, explique por quê.
+
+        a) Por convenção, os nomes de método são iniciados com letra maiúscula, e todas as palavras subsequentes a ele também começam com letra maiúscula.
+           R: Falsa, nomes de métodos começam com letra minusculas e as subsequentes adotam a notação camelo came case Ex:int numeroDecimal = 0;
+
+        b) Uma declaração import não é necessária quando uma classe em um pacote utiliza outra no mesmo pacote.
+           R:Verdadeiro
+
+        c) Parênteses vazios que se seguem a um nome de método em uma declaração indicam que ele não requer nenhum parâmetro para realizar sua tarefa.
+           R:Verdadeiro
+
+        d) Uma variável de tipo primitivo pode ser utilizada para invocar um método.
+           R:Falsa,Variaveis de tipo primitivo não invocam metodos,somente de referencia.
+
+        e) As variáveis declaradas no corpo de um método particular são conhecidas como variáveis de instância e podem ser utilizadas em todos os métodos da classe.
+           R:Falsa,variáveis declaradas no corpo de métodos são consideradas como variáveis locais.
+
+        f) O corpo de todos os métodos é delimitado pelas chaves esquerda e direita ( { e } ).
+           R:Verdadeira
+
+        g) As variáveis locais de tipo primitivo são inicializadas por padrão.
+           R:Falsa, variáveis locais de tipo primitivo requerem inicializadas por padrão.
+
+        h) As variáveis de instância de tipo por referência são inicializadas por padrão com o valor null .
+           R:Verdadeiro
+
+        i) Qualquer classe que contém public static void main(String[] args) pode ser usada para executar um aplicativo.
+           R:Verdadeiro
+
+        j) O número de argumentos na chamada de método deve corresponder ao de itens na lista de parâmetros da declaração desse método.
+           R:Verdadeiro
+
+        k) Os valores de ponto flutuante que aparecem no código-fonte são conhecidos como literais de ponto flutuante e são tipos float por	padrão.
+           R:Falsa. Esses literais são de tipo double por padrão.
+*/
+}
