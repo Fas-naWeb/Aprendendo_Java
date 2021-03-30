60ddc7a00b56f19e45139cc5aedad1b252359c55
oem <fasbaweb2020@gmail.com>
2021-03-30 12:26:03 -0300

    Exercicio de revisão 036

---
+++ Aprendendo_Java/src/Capitulo_002/Exercicio_036.java
@@ -0,0 +1,15 @@
+package Capitulo_002;
+/*
+   Autor:Fabiano Alves Santos
+   Email:fasnaweb2020@gmail.com
+   Data: 30/03/2021
+*/
+public class Exercicio_036 {
+
+	/*
+	    Qual é a diferença entre uma variável local e uma variável de instância?
+	     R:Uma variável local é declarada no corpo de um método e só pode ser utilizada do ponto em que isso acontece até o fim da declaração
+         do método. Uma variável de instância é declarada em uma classe, mas não no corpo de qualquer um dos métodos dessa classe. Além
+         disso, as variáveis de instância são acessíveis a todos os métodos da classe
+	 */
+}
